95e130e7a736c9fedb04556d0522b219929f0d28
emhng <contactemilyhuang@gmail.com>
2022-11-10 12:49:48 -0800

    Deploy

---
+++ _next/static/JHWDBCAaJDsb73vNb6oom/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/213-ecb3fcf95c65033f.js","static/chunks/pages/index-42719a6e09b93789.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
